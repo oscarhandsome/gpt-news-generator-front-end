e15a099e7cdb8f2fe1e9607323c8b96c80077283
Askhat Bagaviyev <altair5555@gmail.com>
2023-09-11 18:01:01 +0600

    updated about us

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -6,30 +6,72 @@
 </script>
 
 <template>
-  <div>About Us</div>
-  <div>
+  <p class="text-2xl font-bold mb-5"><b>About Us</b></p>
+  <div class="mb-10">
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, sed
-      esse animi nulla ex minus ducimus. Vel iste distinctio velit fugit earum
-      esse debitis eveniet ad repellendus mollitia maxime aperiam nihil expedita
-      quos, perspiciatis accusamus excepturi optio corrupti impedit explicabo
-      inventore nemo assumenda incidunt aspernatur? Tenetur qui dolores laborum
-      voluptatibus, perspiciatis aliquam veniam. Tempore, ratione cumque sed
-      sapiente nesciunt eos ipsa ex libero repudiandae ut dolor vero sint, sunt
-      commodi. Cupiditate, alias incidunt adipisci excepturi porro nisi
-      assumenda officiis pariatur velit at accusamus deserunt non perferendis
-      deleniti maiores delectus recusandae, aut blanditiis similique culpa
-      tempore optio. Nostrum accusantium numquam repellat assumenda quisquam
-      doloribus, dignissimos, a inventore nam vitae aliquam aspernatur quos.
-      Itaque animi magni iste laboriosam nulla numquam corrupti earum aut culpa
-      placeat, doloremque, libero expedita, possimus vel aperiam obcaecati
-      incidunt voluptatibus! Nihil at soluta illo commodi voluptatem itaque
-      eligendi quam. Tempore et iste minus dolores maiores itaque culpa, quos
-      officiis cupiditate id illo inventore blanditiis quae cumque rerum a velit
-      voluptatem? Provident, iste, atque ab vel harum facilis in asperiores, eum
-      praesentium earum dolores optio iure aperiam rerum. Aliquid, dolorem culpa
-      tenetur sit reiciendis maxime libero ipsam doloribus voluptatibus harum
-      necessitatibus corporis cumque modi possimus incidunt saepe amet nihil.
+      ---
+      <br />
+      <b class=" text-xl">GPT Chat News Generator Application</b>
+      <br />
+      In today's rapidly evolving information age, there's no denying the power
+      of news. But who said creating news has to be a cumbersome process?
+      Welcome to the GPT Chat News Generator Application - your trusted platform
+      for crafting personalized news articles in a jiffy!
+      <br />
+       <b>Our Vision:</b>
+      <br />
+      To empower every individual with the ability to produce high-quality news
+      articles by leveraging the capabilities of advanced language models, all
+      with the ease of a simple chat.
+      <br />
+       <b>What Do We Offer?</b> * 
+      <br />
+      <ul>
+        <li><b>Instant News Generation:</b> Just type
+      in your topic about a famous person, or any theme, and let our system do
+      the magic. You're seconds away from a well-crafted article!</li>
+        <li>* <b>Tailored News: </b> Ever wanted news articles about your favorite
+      celebrities, events, or subjects? Specify your choice, and we'll deliver
+      content tailored just for you.</li>
+        <li>* <b>Flexible Length Control:</b> Whether you want a concise summary or an
+      in-depth exploration, our platform allows you to dictate the length of
+      your news article.</li>
+      </ul>
+      <br />
+       <b>How Does It Work?</b>
+       <br />
+      Leveraging the power of GPT, the world's leading AI language model, our
+      application communicates with the system to generate news articles based
+      on your input. The future of news generation is chat-based, and we're
+      leading the charge.
+      <br />
+       <b>Who Can Use It? </b>
+       <br />
+      Everyone! From curious individuals to professional journalists, educators,
+      and content creators, our platform is designed for anyone who desires
+      fast, reliable, and tailored news.
+      <br />
+       <b>Our Commitment:</b> * 
+       <br />
+       <ul>
+        <li><b>Quality:</b> We're constantly updating
+      and refining our system to ensure top-notch content for our users.</li>
+        <li><b>User Privacy:</b> Your privacy is our top priority. We guarantee that
+      all your inputs are processed securely without any storage or data
+      retention.</li>
+        <li><b>Innovation:</b> In the ever-evolving landscape of AI, we're dedicated
+      to continuously innovating and enhancing our platform's capabilities.</li>
+       </ul>
+      <br />
+      ---
+      <br />
+      <b>Join us on this exciting journey of reimagining news generation!</b>
+      <br />
+      ---
+      <br />
+      For any queries or feedback, feel free to [contact us](#).
+      <br />
+      © 2023 GPT Chat News Generator Application. All rights reserved.
     </p>
   </div>
 </template>
