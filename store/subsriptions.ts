1e1c79a7182dfaffd52b7bc17bb883ebcad170d0
Askhat Bagaviyev <altair5555@gmail.com>
2023-09-13 19:42:06 +0600

    fixed sub requets

--- store/subsriptions.ts
+++ store/subsriptions.ts
@@ -3,13 +3,13 @@ import { UserPayloadInterface } from 'types'
 // import { useToast } from 'tailvue'
 import Api from '~/services/api'
 
-import { useMyFetch } from '@/composables/useMyFetch'
+// import { useMyFetch } from '@/composables/useMyFetch'
 
 // const $toast = useToast()
 // const { $toast } = useNuxtApp()
 
 // const baseURL = useRuntimeConfig().public.baseURL
-const BASE_URL = 'http://localhost:8000/api/v1'
+// const BASE_URL = 'http://localhost:8000/api/v1'
 
 export const useSubscriptionStore = defineStore('subscription', {
   state: () => ({
@@ -23,9 +23,10 @@ export const useSubscriptionStore = defineStore('subscription', {
   actions: {
     async getSubscriptions() {
       this.isLoading = true
-      const { data, pending }: any = await Api.get('/subscriptions')
+      const { data, pending, error }: any = await Api.get('/subscriptions')
       if (data.value) this.subscriptions = data.value.data.data
-      this.isLoading = false
+      this.isLoading = pending.value
+      if (error.value) console.error('error', error)
     },
     // async getSubscriptions() {
     //   // $toast.show('this is a test')
@@ -58,30 +59,28 @@ export const useSubscriptionStore = defineStore('subscription', {
     async getMySubscriptions() {
       // $toast.show('this is a test')
       // useFetch from nuxt 3
-      const token = useCookie('token')
-      const { data, pending }: any = await useFetch(
-        `${BASE_URL}/subscriptions/my-subscription`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token.value}`,
-            'Content-Type': 'application/json',
-          },
-        },
+      // const token = useCookie('token')
+      // const { data, pending }: any = await useFetch(
+      //   `${BASE_URL}/subscriptions/my-subscription`,
+      //   {
+      //     method: 'GET',
+      //     headers: {
+      //       Authorization: `Bearer ${token.value}`,
+      //       'Content-Type': 'application/json',
+      //     },
+      //   },
+      // )
+      const { data, pending, error }: any = await Api.get(
+        '/subscriptions/my-subscription',
       )
-      this.isLoading = pending
-
-      console.log('this.isLoading', this.isLoading)
-      console.log('data.value', data.value)
-
-      if (data.value.status === 'success') {
+      if (data.value && data.value.status === 'success') {
         this.success = true
         this.mySubscription = data.value.data.data
       }
-
+      this.isLoading = pending.value
       console.log('data', data)
-
-      if (!data.value) throw new Error('Something went wrong!')
+      // if (!data.value) throw new Error('Something went wrong!')
+      if (error.value) console.error('error', error)
     },
   },
 })
