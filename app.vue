88060ebceaf2f26b9adbefb1cd796c7bb5cbad44
oscarhandsome <altair5555@gmail.com>
2023-11-03 20:48:07 +0600

    added sentry

--- app.vue
+++ app.vue
@@ -5,6 +5,12 @@ useSchemaOrg([
   }),
   defineWebPage(),
 ])
+
+// const { $sentryCaptureException } = useNuxtApp()
+
+// onMounted(() => {
+//   $sentryCaptureException('Test exception')
+// })
 </script>
 
 <template>
@@ -12,3 +18,18 @@ useSchemaOrg([
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+body::-webkit-scrollbar {
+  width: 0.5em;
+}
+
+body::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color: darkgrey;
+  outline: 1px solid slategrey;
+}
+</style>
