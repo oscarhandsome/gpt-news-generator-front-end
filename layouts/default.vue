4261623c2652ad90ff0b69df3a09125f91c50213
oscarhandsome <altair5555@gmail.com>
2023-12-16 16:04:44 +0600

    clear default template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,6 @@
-<script lang="ts" setup>
-// import { storeToRefs } from 'pinia' // import storeToRefs helper hook from pinia
-// import { useAuthStore } from '~/store/auth' // import the auth store we just created
-
-// const router = useRouter()
-
-// const { logUserOut } = useAuthStore() // use authenticateUser action from  auth store
-// const { isAuthenticated } = storeToRefs(useAuthStore()) // make isAuthenticated state reactive with storeToRefs
-
-// const logout = () => {
-//   logUserOut()
-//   router.push('/auth/login')
-// }
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <!-- <ul>
-    <li
-      v-if="!isAuthenticated"
-      class="loginBtn text-red-500 p-2"
-      style="float: right"
-    >
-      <nuxt-link to="/auth/login">Login</nuxt-link>
-    </li>
-    <li
-      v-if="isAuthenticated"
-      class="loginBtn text-red-500 p-2"
-      style="float: right"
-    >
-      <nuxt-link @click="logout">Logout</nuxt-link>
-    </li>
-  </ul> -->
   <div class="flex flex-col min-h-screen dark:bg-gray-900 dark:text-white">
     <TheHeader />
     <main class="flex flex-col flex-grow">
