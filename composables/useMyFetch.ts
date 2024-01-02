6b319c77de4b0c982431100ab51c1efd57303d0d
oscarhandsome <altair5555@gmail.com>
2024-01-02 19:33:25 +0600

    error handler

--- composables/useMyFetch.ts
+++ composables/useMyFetch.ts
@@ -1,11 +1,12 @@
-export const useMyFetch: typeof useFetch = (request, opts?) => {
+export const useMyFetch: typeof useFetch = async (request, opts?) => {
+  const { $toaster } = useNuxtApp()
   const config = useRuntimeConfig()
   const token = useCookie('token')
 
   // return useFetch(request, { baseURL: config.public.baseURL, ...opts })
 
   try {
-    return useFetch(request, {
+    const response = await useFetch(request, {
       baseURL: config.public.baseURL,
       headers: {
         Authorization: `Bearer ${token.value}`,
@@ -14,6 +15,21 @@ export const useMyFetch: typeof useFetch = (request, opts?) => {
       },
       ...opts,
     })
+    const { data, pending, error }: any = response
+    console.log('data, pending, error', data, pending, error)
+
+    if (error && error.value) {
+      console.error(error.value.data)
+      $toaster.error({
+        title: `Error`,
+        message: 'Something went wrong',
+      })
+      // $toaster.error({
+      //   title: `Error, status: ${error.value.data.status}, code: ${error.value.data.error.statusCode}`,
+      //   message: error.value.data.message,
+      // })
+    }
+    return response
   } catch (error) {
     return error
   }
