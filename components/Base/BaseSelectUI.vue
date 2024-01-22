c3d7d21ad6cd77542da732cc15d3b8edd45c9d9c
Askhat Bagaviyev <altair5555@gmail.com>
2024-01-22 12:01:34 +0600

    change order

--- components/Base/BaseSelectUI.vue
+++ components/Base/BaseSelectUI.vue
@@ -80,8 +80,8 @@ watch(selectedItem, (_val) => {
             class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
           >
             <ListboxOption
-              v-slot="{ active, selected }"
               v-for="item in items"
+              v-slot="{ active, selected }"
               :key="item.name"
               :value="item"
               as="template"
