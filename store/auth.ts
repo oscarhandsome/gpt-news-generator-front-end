7252e5e3ea399a4c239fa39b736ea60414e74d91
oscarhandsome <altair5555@gmail.com>
2024-01-24 19:36:56 +0600

    aatempt fix email confirm5

--- store/auth.ts
+++ store/auth.ts
@@ -175,28 +175,21 @@ export const useAuthStore = defineStore('auth', {
       }, 3500)
     },
     async emailConfirm(payload: string) {
-      // const { data, pending, error }: any = await Api.post(
-      //   `/users/emailConfirm/${payload}`,
-      // )
-      const { data, pending, error, refresh }: any = await useFetch(
-        `https://gpt-news-generator.onrender.com/users/emailConfirm/${payload}`,
-        {
-          method: 'post',
-          headers: { 'Content-Type': 'application/json' },
-        },
+      const { data, pending, error }: any = await Api.post(
+        `/users/emailConfirm/${payload}`,
       )
-      console.log('data.value', data.value)
-      console.log('pending.value', pending.value)
-      console.log('error.value', error.value)
-      console.log('refresh.value', refresh.value)
-      if (data.value && data.value.status === 'success') {
-        const token = useCookie('token') // useCookie new hook in nuxt 3
-        token.value = data?.value?.token // set token to cookie
-        this.isAuthenticated = true // set isAuthenticated  state value to true
-        this.user = data.value.data.user
-        localStorage.setItem('user', JSON.stringify(this.user))
-        this.success = true
-      }
+      // console.log('data.value', data.value)
+      // console.log('pending.value', pending.value)
+      // console.log('error.value', error.value)
+      // console.log('refresh.value', refresh.value)
+      // if (data.value && data.value.status === 'success') {
+      //   const token = useCookie('token') // useCookie new hook in nuxt 3
+      //   token.value = data?.value?.token // set token to cookie
+      //   this.isAuthenticated = true // set isAuthenticated  state value to true
+      //   this.user = data.value.data.user
+      //   localStorage.setItem('user', JSON.stringify(this.user))
+      //   this.success = true
+      // }
 
       // if (error.value) {
       //   console.log('error.value.data', error.value.data)
