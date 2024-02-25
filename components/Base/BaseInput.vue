88352cce052a17646d02bf89fd58cbaa30f3b698
Askhat Bagaviyev <altair5555@gmail.com>
2024-02-25 14:44:08 +0600

    fixed input model

--- components/Base/BaseInput.vue
+++ components/Base/BaseInput.vue
@@ -4,6 +4,7 @@ import type { InputProps } from '@/types'
 defineEmits(['update:modelValue'])
 const props = defineProps<InputProps>()
 const id = props.label.toLocaleLowerCase().replaceAll(' ', '-')
+const value = ref(props.modelValue)
 </script>
 
 <template>
@@ -21,9 +22,9 @@ const id = props.label.toLocaleLowerCase().replaceAll(' ', '-')
     <input
       :id="id"
       :ref="id"
+      v-model="value"
       :name="id"
       :type="type ?? 'text'"
-      :value="modelValue"
       class="border text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 transition-colors w-full p-2.5"
       :class="[
         error
@@ -31,9 +32,7 @@ const id = props.label.toLocaleLowerCase().replaceAll(' ', '-')
           : 'bg-gray-50 border-gray-300 text-gray-900 ',
       ]"
       :placeholder="placeholder"
-      @input="
-        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
-      "
+      @keyup="$emit('update:modelValue', value)"
     />
     <p
       v-if="error"
